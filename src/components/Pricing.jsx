366ad3a7b8cc214b20d394725abb6c0a6dbc63de
Parth Savaliya <parth.savaliya112@gmail.com>
2024-01-17 18:01:42 +0530

    Price page

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,8 +1,84 @@
 import React from 'react'
+import { Col, Container, Row, Button, Accordion } from 'react-bootstrap'
+import { priceFaqArray, pricingPlan } from '../utils/Constant'
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
+    <Container>
+      <Row className='section-1'>
+        <Col>
+          <h1 className='content-1'>
+            Welcome to BlogAPP!
+          </h1>
+          <h1 className='content-2'>
+            Explore our pricing plans to find the best fit for your needs.
+          </h1>
+        </Col>
+      </Row>
+      <Row className='section-2'>
+        <Row>
+          <Col>
+            <h1 className='blog-main'>Pricing Plan</h1>
+          </Col>
+        </Row>
+
+        <div className='grid-container-price'>
+
+          {pricingPlan?.map((price, inx) => (
+            <div key={inx} className='card price-card' xs={12} md={4} lg={3}>
+              <div className='card-main'>
+                <div>
+                  <div className='blog-title'>
+                    {price?.title}
+                  </div>
+                  <div className='price'>
+                    {`$${price?.cost}/month`}
+                  </div>
+                  <hr />
+                  <ul className='price-desc-list'>
+                    {price?.desc?.map((de, index) => (
+                      <li className='price-desc' key={index}>{de}</li>
+                    ))}
+                  </ul>
+
+
+                </div>
+              </div>
+            </div>
+          ))}
+
+        </div>
+        <div className='text-center'>
+          <Button className='started-btn'>
+            Subscribe
+          </Button>
+        </div>
+
+      </Row>
+      <Row className='section-3'>
+        <Row>
+          <Col>
+            <h2 className='faq'>Frequently Asked Questions (FAQs)
+            </h2>
+          </Col>
+        </Row>
+        <Row className='faq-list'>
+          <Accordion defaultActiveKey="0" flush>
+            {priceFaqArray?.map((faq, index) => (
+              <Accordion.Item eventKey={index}>
+                <Accordion.Header className="faq-question">{faq?.question}</Accordion.Header>
+                <Accordion.Body>
+                  {faq?.answer}
+                </Accordion.Body>
+              </Accordion.Item>
+            ))}
+
+
+          </Accordion>
+        </Row>
+
+      </Row>
+    </Container>
   )
 }
 
---
+++ src/utils/Constant.jsx
@@ -0,0 +1,71 @@
+export const pricingPlan = [
+    {
+        title: "Free Plan",
+        cost: "0",
+        desc: ["Access to a limited selection of articles", "Basic community support", "Ad-supported experience"]
+    },
+    {
+        title: "Basic Plan",
+        cost: "9.99",
+        desc: ["Ad-free reading experience", "Access to premium articles", "Access to premium articles", "Monthly newsletter subscription"]
+    },
+    {
+        title: "Pro Plan",
+        cost: "19.99",
+        desc: ["Ad-free reading experience", "Full access to all premium articles", "Priority customer support", "Exclusive monthly webinars", "Access to member-only forums"]
+    },
+    {
+        title: "Premium Plan",
+        cost: "29.99",
+        desc: ["Ad-free reading experience", "Unlimited access to premium", "Archived content", "24/7 VIP customer support", "Early access to new features", "Personalized content recommendations", "Quarterly in-depth reports"]
+    }
+
+]
+
+export const pricingContent = [
+    {
+        title: "Rich Content Library",
+        description: "Access a vast library of articles covering diverse topics. From technology trends to lifestyle advice, [Your Blog Name] offers a wealth of knowledge at your fingertips.",
+    },
+    {
+        title: "Community Engagement",
+        description: "Connect with like-minded individuals, authors, and creators. Engage in discussions, share insights, and become part of a thriving community.",
+    },
+    {
+        title: "Ad-Free Experience",
+        description: "Enjoy a seamless reading experience without interruptions. Our plans ensure an ad-free environment for immersive content consumption.",
+    },
+    {
+        title: "Exclusive Content",
+        description: "Unlock premium articles, behind-the-scenes content, and early releases. Our premium plans provide exclusive access to enrich your reading experience.",
+    },
+    {
+        title: "Customer Support",
+        description: "Receive prompt and personalized support from our dedicated customer service team. Your satisfaction is our priority.",
+    },
+    {
+        title: "Need Customization?",
+        description: "Tailor a plan to meet your specific requirements. Contact our sales team to discuss custom features, enterprise solutions, and special arrangements.",
+    },
+];
+
+
+export const priceFaqArray = [
+    {
+        question: "Is there a free trial available?",
+        answer: "Yes, we offer a 7-day free trial for our Pro and Premium plans. During the trial period, you'll have access to all the premium features without any charges. It's a great opportunity to explore and experience the full range of benefits our plans provide. After the trial period ends, you can choose to continue with a paid plan to retain access to premium features.",
+    },
+    {
+        question: "Can I change my plan later?",
+        answer: "Certainly! We understand that your needs may evolve over time. You can upgrade or downgrade your plan at any time to better align with your requirements. Changes will take effect at the beginning of your next billing cycle, ensuring a seamless transition. Our flexible plan options allow you to adjust your subscription based on your current preferences and usage.",
+    },
+    {
+        question: "How do I cancel my subscription?",
+        answer: "Canceling your subscription is hassle-free. Simply navigate to your account settings, and you'll find an option to cancel your subscription. There are no long-term commitments, and you have the freedom to stop your subscription whenever you choose. Once canceled, you won't be billed for the upcoming billing cycles, and your access to premium features will continue until the end of your current subscription period.",
+    },
+    {
+        question: "Do you offer refunds?",
+        answer: "Yes, we have a 30-day refund policy to ensure your satisfaction. If, within the first 30 days of your subscription date, you find that our service doesn't meet your expectations or requirements, you can contact our customer support team. We'll process a full refund for your subscription cost. Our goal is to provide you with a risk-free experience and ensure you are completely satisfied with our service.",
+    },
+
+]
