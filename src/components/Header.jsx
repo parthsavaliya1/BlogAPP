81ebba2e3f769f3caab20c426f300f7110e70677
Parth Savaliya <parth.savaliya112@gmail.com>
2024-01-18 16:59:52 +0530

    logout dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,80 +1,77 @@
-import React, { useContext } from 'react'
-import { Button, Container, Nav, Navbar, Stack } from 'react-bootstrap'
-import BlogApp from '../assets/blog.png'
-import { AuthContext } from '../context/AuthContext'
+import React, { useContext } from 'react';
+import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import BlogApp from '../assets/blog.png';
+import { AuthContext } from '../context/AuthContext';
 
 const Header = () => {
-    const { user } = useContext(AuthContext);
-    const showUserName = () => {
-        return `${user?.user?.firstName} ${user?.user?.lastName}`
-    }
+    const { user,setUser } = useContext(AuthContext);
+
+    const showUserName = () => `${user?.user?.firstName} ${user?.user?.lastName}`;
+
     const showProfileImage = () => {
         if (user?.user?.profilePicture) {
-            return (
-                <div>
-                    <img src={user?.user?.profilePicture} />
-                </div>
-            )
-
+            return <img src={user?.user?.profilePicture} alt="Profile" />;
         } else {
             return (
-                <div className='profile-container'>
-                    <div className='profile-avatar'>
+                <div className="profile-container">
+                    <div className="profile-avatar">
                         <span>{user?.user?.firstName?.[0]}</span>
                         <span>{user?.user?.lastName?.[0]}</span>
                     </div>
                 </div>
-
-            )
-
+            );
         }
-    }
-    return (
-        <div className='header'>
+    };
 
-            <Navbar sticky='top' collapseOnSelect expand="lg" className="navbar-main ">
+    const handleLogout = () => {
+        setUser(null)
+        localStorage.removeItem('user');
+    }
+
+    return (
+        <div className="header">
+            <Navbar sticky="top" collapseOnSelect expand="lg" className="navbar-main">
                 <Container>
-                    <Navbar.Brand href="/"><img className='logo-img' src={BlogApp} /></Navbar.Brand>
+                    <Navbar.Brand href="/">
+                        <img className="logo-img" src={BlogApp} alt="Blog Logo" />
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link className='header-link' href="/about">About</Nav.Link>
-                            <Nav.Link className='header-link' href="/pricing">Pricing</Nav.Link>
-                            <Nav.Link className='header-link' href="/contact">Contact</Nav.Link>
-
-
+                            <Nav.Link className="header-link" href="/about">
+                                About
+                            </Nav.Link>
+                            <Nav.Link className="header-link" href="/pricing">
+                                Pricing
+                            </Nav.Link>
+                            <Nav.Link className="header-link" href="/contact">
+                                Contact
+                            </Nav.Link>
                         </Nav>
                         {user ? (
-                            <Nav className='profile-nav'>
-                                <Nav.Link href="#">
-                                    <div className='header-link'>
-                                        {showUserName()}
-                                    </div>
-                                </Nav.Link>
+                            <Nav className="profile-nav">
+                                <NavDropdown title={showUserName()} id="collasible-nav-dropdown">
+                                    <NavDropdown.Item onClick={() => handleLogout()}>Logout</NavDropdown.Item>
+                                </NavDropdown>
                                 <Nav.Link eventKey={2} href="#">
-                                    <div >
-                                        {showProfileImage()}
-                                    </div>
+                                    <div>{showProfileImage()}</div>
                                 </Nav.Link>
                             </Nav>
                         ) : (
                             <Nav>
                                 <Nav.Link href="/register">
-                                    <Button className='register-btn'>Register</Button>
+                                    <Button className="register-btn">Register</Button>
                                 </Nav.Link>
                                 <Nav.Link eventKey={2} href="/login">
-                                    <Button className='login-btn'>Login</Button>
+                                    <Button className="login-btn">Login</Button>
                                 </Nav.Link>
                             </Nav>
                         )}
                     </Navbar.Collapse>
                 </Container>
-            </Navbar >
-        </div >
-    )
-}
+            </Navbar>
+        </div>
+    );
+};
 
-export default Header
-
-
-
+export default Header;
